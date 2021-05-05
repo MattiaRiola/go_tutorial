cbbd7fa42a4dea8de42267e84b93bee462eb55c7
Bacarus <code.bacarus@gmail.com>
2021-05-05 18:44:13 +0200

    test: play with test

--- mongodb-fleetlogs/mongo_test.go
+++ mongodb-fleetlogs/mongo_test.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,15 +16,53 @@ const (
 	MONGOURI_TEST        = "mongodb://localhost:27017"
 	MONGO_DB_RESULT_TEST = "fleetLogResultTest"
 	MONGO_DB_STATUS_TEST = "fleetLogStatusTest"
+	MONGO_TEST           = true
 )
 
-func MongoConnectionTest(t *testing.T) {
-	mongoW, err := NewMongoLogWriter("", context.TODO())
+var testFunctions = [...]func(*testing.T){
+	testMyAlwaysOkFunc,
+	// testMyAlwaysFailFunc,
+	testTestMongoConnection,
+}
+
+func FunctionName(f interface{}) string {
+	fullName := runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+	elements := strings.Split(fullName, ".")
+	return elements[len(elements)-1]
+}
+func TestAll(t *testing.T) {
+	if !MONGO_TEST {
+		t.Skip("Mongo tests not requested")
+	}
+	for _, f := range testFunctions {
+		t.Run(FunctionName(f), f)
+	}
+	t.Run(FunctionName(testMyAlwaysFailFunc), testMyAlwaysFailFunc)
+}
+func TestAllOk(t *testing.T) {
+	if !MONGO_TEST {
+		t.Skip("Mongo tests not requested")
+	}
+	for _, f := range testFunctions {
+		t.Run(FunctionName(f), f)
+	}
+}
+func testTestMongoConnection(t *testing.T) {
+	_, err := NewMongoLogWriter("", context.TODO())
 	require.NotNil(t, err)
 
-	mongoW, err = NewMongoLogWriter(MONGOURI_TEST, context.TODO())
+	mongoW, err := NewMongoLogWriter(MONGOURI_TEST, context.TODO())
 	require.Nil(t, err)
 
 	assert.Equal(t, mongoW.db.Name(), "fleetLog")
 
+}
+
+func testMyAlwaysOkFunc(t *testing.T) {
+	fmt.Println("Testing the always ok func")
+	assert.True(t, true)
+}
+func testMyAlwaysFailFunc(t *testing.T) {
+	fmt.Println("Testing the always fail func")
+	assert.True(t, false)
 }
